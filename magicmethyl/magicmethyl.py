53d32b4c059aaa342260f7439154605a372caff6
Sam Mun <therealsam@berkeley.edu>
2022-04-23 16:30:02 -0700

    added "methylate"

--- magicmethyl/magicmethyl.py
+++ magicmethyl/magicmethyl.py
@@ -1,10 +1,56 @@
-def convert(my_name):
-    """
-    Print a line about converting a notebook.
-    Args:
-        my_name (str): person's name
-    Returns:
-        None
-    """
+from rdkit import Chem
 
-    print(f"I'll convert a notebook for you some day, {my_name}.")
+def methylate(smi: str) -> list[str]:
+    """Generates all unique methylations including stereoisomers.
+
+    Examples
+    --------
+    analogs = methylate('CC1CCCCC1')
+
+    Parameters
+    ----------
+    smi: str
+        The input SMILES string.
+
+    Returns
+    -------
+    list[str]
+        A list containing all unique methylations as SMILES strings."""
+    mol = Chem.MolFromSmiles(smi)
+    num_atoms = mol.GetNumAtoms()
+    analogs = []
+
+    for i, atom in enumerate(mol.GetAtoms()):
+        # require the atom to be methylated has hydrogens
+        if atom.GetTotalNumHs() > 0:
+            # carbons are treated differently from heteroatoms due to stereochem
+            if atom.GetAtomicNum() == 6:
+                # use RWMol to edit the molecule
+                analog = Chem.RWMol(mol)
+                analog.AddAtom(Chem.Atom(6))
+                analog.AddBond(i, num_atoms, Chem.BondType.SINGLE)
+                # if the atom is not sp3, there cannot be a stereocenter
+                if atom.GetHybridization() != Chem.HybridizationType.SP3:
+                    analog = analog.GetMol()
+                    analogs.append(Chem.MolToSmiles(analog))
+                # if there is a possible stereocenter, we generate both possible
+                #  mols and compare their SMILES strings to check for uniqueness
+                else:
+                    analog.GetAtomWithIdx(i).SetChiralTag(Chem.ChiralType.CHI_TETRAHEDRAL_CW)
+                    cw_isomer = analog.GetMol()
+                    analog.GetAtomWithIdx(i).SetChiralTag(Chem.ChiralType.CHI_TETRAHEDRAL_CCW)
+                    ccw_isomer = analog.GetMol()
+                    if Chem.MolToSmiles(cw_isomer) == Chem.MolToSmiles(ccw_isomer):
+                        analogs.append(Chem.MolToSmiles(cw_isomer))
+                    else:
+                        analogs.append(Chem.MolToSmiles(cw_isomer))
+                        analogs.append(Chem.MolToSmiles(ccw_isomer))
+            # heteroatoms do not require a stereochem check
+            if atom.GetAtomicNum() in [7, 8, 16]:
+                analog = Chem.RWMol(mol)
+                analog.AddAtom(Chem.Atom(6))
+                analog.AddBond(i, num_atoms, Chem.BondType.SINGLE)
+                analog = analog.GetMol()
+                analogs.append(Chem.MolToSmiles(analog))
+            
+    return list(set(analogs))
--- tests/test_magicmethyl.py
+++ tests/test_magicmethyl.py
@@ -1,9 +1,14 @@
 # import pytest
 from magicmethyl import magicmethyl
 
-
-def test_convert(capsys):
-    """Correct my_name argument prints"""
-    magicmethyl.convert('Jill')
-    captured = capsys.readouterr()
-    assert 'Jill' in captured.out
+def test_methylate():
+    """Generates the correct methylated isomers"""
+    analogs = magicmethyl.methylate('ONC(=O)C1CC=CC1')
+    assert len(analogs) == 6
+    assert 'C[C@@]1(C(=O)NO)CC=CC1' in analogs
+    assert 'CONC(=O)C1CC=CC1' in analogs
+    assert 'C[C@H]1C=CCC1C(=O)NO'  in analogs
+    assert 'CN(O)C(=O)C1CC=CC1' in analogs
+    assert 'CC1=CCC(C(=O)NO)C1' in analogs
+    assert 'C[C@@H]1C=CCC1C(=O)NO' in analogs
+    
