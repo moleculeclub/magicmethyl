7c4c4fb80a1937cfd7532169cd7913cccfb5cce8
Sam Mun <therealsam@berkeley.edu>
2022-05-04 18:19:07 -0700

    update methylation stuff

--- magicmethyl/magicmethyl.py
+++ magicmethyl/magicmethyl.py
@@ -20,6 +20,7 @@ def methylate(smi: str):
     list[str]
         A list containing all unique methylations as SMILES strings."""
     mol = Chem.MolFromSmiles(smi)
+    mol_h = Chem.AddHs(mol)
     num_atoms = mol.GetNumAtoms()
     analogs = []
 
@@ -29,28 +30,41 @@ def methylate(smi: str):
             # carbons are treated differently from heteroatoms due to stereochem
             if atom.GetAtomicNum() == 6:
                 # use RWMol to edit the molecule
-                analog = Chem.RWMol(mol)
-                analog.AddAtom(Chem.Atom(6))
-                analog.AddBond(i, num_atoms, Chem.BondType.SINGLE)
-                # find if the atom has any other methyls on it
+                hyd = None
+                for neighbor in mol_h.GetAtomWithIdx(i).GetNeighbors():
+                    if neighbor.GetSymbol() == 'H':
+                        hyd = neighbor
+                analog = Chem.RWMol(mol_h)
+                analog.ReplaceAtom(hyd.GetIdx(), Chem.Atom(6))
+                # # leftover from old implementation
+                # analog.AddAtom(Chem.Atom(6))
+                # analog.AddBond(i, num_atoms, Chem.BondType.SINGLE)
+                # # find if the atom has any other methyls on it
                 has_methyls = False
                 for neighbor in atom.GetNeighbors():
                     if (neighbor.GetSymbol() == 'C' and
                        neighbor.GetTotalNumHs() == 3):
                         has_methyls = True
                 # if the atom is not sp3, there cannot be a stereocenter
+                # if the atom has a methyl group, there will be no stereocenter
                 if (atom.GetHybridization() != Chem.HybridizationType.SP3 or
                    atom.GetTotalNumHs() == 3
                    or has_methyls):
                     analog = analog.GetMol()
+                    analog = Chem.RemoveAllHs(analog)
+                    # analog.UpdatePropertyCache()
                     analogs.append(Chem.MolToSmiles(analog))
                 # if there is a possible stereocenter, we generate both possible
                 #  mols and compare their SMILES strings to check for uniqueness
                 else:
                     analog.GetAtomWithIdx(i).SetChiralTag(CHI_TETRAHEDRAL_CW)
                     cw_isomer = analog.GetMol()
+                    cw_isomer = Chem.RemoveAllHs(cw_isomer)
+                    # cw_isomer.UpdatePropertyCache()
                     analog.GetAtomWithIdx(i).SetChiralTag(CHI_TETRAHEDRAL_CCW)
                     ccw_isomer = analog.GetMol()
+                    ccw_isomer = Chem.RemoveAllHs(ccw_isomer)
+                    # ccw_isomer.UpdatePropertyCache()
                     if (Chem.MolToSmiles(cw_isomer) ==
                        Chem.MolToSmiles(ccw_isomer)):
                         analogs.append(Chem.MolToSmiles(cw_isomer))
@@ -63,6 +77,7 @@ def methylate(smi: str):
                 analog.AddAtom(Chem.Atom(6))
                 analog.AddBond(i, num_atoms, Chem.BondType.SINGLE)
                 analog = analog.GetMol()
+                # analog.UpdatePropertyCache()
                 analogs.append(Chem.MolToSmiles(analog))
 
     return list(set(analogs))
--- tests/test_magicmethyl.py
+++ tests/test_magicmethyl.py
@@ -6,16 +6,13 @@ def test_methylate():
     """Generates the correct methylated isomers"""
     analogs = magicmethyl.methylate('ONC(=O)C1CC=CC1')
     assert len(analogs) == 6
-    assert 'C[C@@]1(C(=O)NO)CC=CC1' in analogs
-    assert 'CONC(=O)C1CC=CC1' in analogs
-    assert 'C[C@H]1C=CCC1C(=O)NO' in analogs
-    assert 'CN(O)C(=O)C1CC=CC1' in analogs
-    assert 'CC1=CCC(C(=O)NO)C1' in analogs
-    assert 'C[C@@H]1C=CCC1C(=O)NO' in analogs
 
     analogs = magicmethyl.methylate('CN(CCN1CCCC1)C(=O)CC1=CC=C(Cl)C(Cl)=C1')
     assert len(analogs) == 14
 
-    # this test fails due to inability to recognize symmetry in methyl groups
     analogs = magicmethyl.methylate('CCCC1=CC2=C(OC=CC2=O)C=C1OC(C)=O')
     assert len(analogs) == 9
+
+    analogs = magicmethyl.methylate('CC1=CC(C(=O)N2CCC[C@@H](COC3=CC(C)=C(F)C=C'
+                                    '3)C2)=C(C=C1)N1N=CC=N1')
+    assert len(analogs) == 21
